de4145fd34c9ac34bc5e79a4e23d847ed1bd45ee
Patrick DeMarco <pfdemarco@hotmail.com>
2021-06-10 22:19:43 -0400

    bro gto access to the api now to react properly

---
+++ themet/src/components/paintings.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+// axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects')
+//   .then(function (response) {
+//     // handle success
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+//   });
+
+  axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects/45734')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+  
+  
